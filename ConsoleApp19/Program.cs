056e917c261ee6ed591dbc6df7391361f32fcac2
John Hinz <jhinz402@hotmail.com>
2022-04-16 11:24:02 -0400

    Initial Commit

--- ConsoleApp19/Program.cs
+++ ConsoleApp19/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Diagnostics;
+
+
+    Stopwatch sw = new Stopwatch();
+    sw.Start();
+    int result = AddSomeNumbersAsync().Result;
+    sw.Stop();
+
+    Console.WriteLine($"{result} took {sw.Elapsed} time");
+
+    async Task<int> AddSomeNumbersAsync()
+    {
+        Task<int> a = SomeLongRunningMethodAAsync();
+        Task<int> b = SomeOtherLongRunningMethodBAsync();
+
+        return await a + await b;
+    }
+
+
+    Task<int> SomeLongRunningMethodAAsync()
+    {
+        return Task.Run<int>(() =>
+        {
+            Thread.Sleep(3000);
+            return 3000;
+        });
+    }
+
+    Task<int> SomeOtherLongRunningMethodBAsync()
+    {
+        return Task.Run<int>(() =>
+        {
+            Thread.Sleep(5000);
+            return 5000;
+        });
+    }
